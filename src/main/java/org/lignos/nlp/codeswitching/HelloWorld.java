b4434fe3725467e19ecc05f2f7a28750f4de3a1d
Constantine Lignos <constantine@lignos.org>
2015-02-17 12:42:51 -0500

    Add stub maven project.

---
+++ src/main/java/org/lignos/nlp/codeswitching/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.lignos.nlp.codeswitching;
+
+/**
+ * Created by lignos on 2/17/15.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello world!\n");
+    }
+}
